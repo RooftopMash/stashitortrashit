486bc0628be78b3078639654ffd1a655da45d42c
RooftopMash <borulelo@gmail.com>
2025-05-18 01:05:03 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Stash or Trash!</h1>
+      <p>This is the world's best brand, product, and company rating platform.</p>
+    </div>
+  );
+}
